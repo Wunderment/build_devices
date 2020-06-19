f11cc6620ea6b27af483f26f45b5c32c4567b8ec
Greg <greg@toolstack.com>
2020-06-19 19:25:58 -0400

    Add new make options to include extra partitions in the OTA.
    As well as fix a typo in the directory name.

--- fajita/build/device-build-functions.sh
+++ fajita/build/device-build-functions.sh
@@ -8,16 +8,26 @@ function build_wos {
 		
 		# We're using the sparse image here otherwise LOS will try and use the SEPolicy files from the ext
 		# version and throw policy errors as they are from OOS.
+		echo "# Add vendor image to the OTA and vbmeta hashtree." > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
 		echo "BOARD_PREBUILT_VENDORIMAGE := /home/WundermentOS/devices/fajita/blobs/images_raw/vendor.img" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
 		
 		echo "AB_OTA_PARTITIONS += vendor" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
-		echo "BOARD_AVB_ALGORITHM := SHA256_RSA2048" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
+		
+		echo "" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
+		echo "# Set the AVB key and hash algorithm." > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
 		echo "BOARD_AVB_KEY_PATH := /home/WundermentOS/.android-certs/releasekey.x509.pem" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
+		echo "BOARD_AVB_ALGORITHM := SHA256_RSA2048" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
+
+		echo "" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
+		echo "# Include the rest of the prebuilt partitions." > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
+		echo "BOARD_PREBUILT_IMAGES := abl aop bluetooth cmnlib cmnlib64 devcfg dsp fw_4j1ed fw_4u1ea hyp india keymaster LOGO modem oem_stanvbk qupfw reserve storsec tz xbl xbl_config" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
+		echo "BOARD_PREBUILT_IMAGES_PATH := /home/WundermentOS/devices/fajita/blobs/images" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
+		echo "AB_OTA_PARTITIONS += abl aop bluetooth cmnlib cmnlib64 devcfg dsp fw_4j1ed fw_4u1ea hyp india keymaster LOGO modem oem_stanvbk qupfw reserve storsec tz xbl xbl_config" > ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/fajita/BoardConfig.mk
 	fi
 
 	# We need to remove the flag that disables the partition verification during boot if it hasn't been already
 	# in the sdm845 common code.
-	if ! grep "#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS" ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/sdm845/BoardConfigCommon.mk > /dev/null; then
+	if ! grep "#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS" ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/sdm845-common/BoardConfigCommon.mk > /dev/null; then
 		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2/' ~/android/lineage-$LOS_BUILD_VERSION/device/oneplus/sdm845-common/BoardConfigCommon.mk
 	fi
 
