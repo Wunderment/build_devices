cdd7c4c10055d28ce4f499b77e8e5693c39b1579
Greg <greg@toolstack.com>
2020-07-02 17:52:38 -0400

    Add guacamoleb device files
    aka OnePlus 7

---
+++ guacamoleb/blobs/extract-stock-os-blobs.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Get the device list and build types.
+source ~/.WundermentOS/devices.sh
+
+# Get the device name from the parent directory of this script's real path.
+DEVICE=$(basename $(dirname $(dirname $(realpath $0))))
+VENDOR=oneplus
+
+# A device name may have a special case where we're building multiple versios, like for LOS 16
+# and 17.  In these cases an extra modifier on the device name is added that starts with a '_'
+# so for example dumpling_17 to indicate to build LOS 17 for dumpling.  In these cases we need
+# to leave the modifier on $DEVICE so logs and other commands are executed in the right directory
+# but for the acutal LOS build, we need to strip it off.  So do so now.
+LOS_DEVICE=`echo $DEVICE | sed 's/_.*//'`
+
+# Find out which version of LinageOS we're going to build for this device.
+WOS_BUILD_VAR=WOS_BUILD_VER_${DEVICE^^}
+LOS_BUILD_VERSION=${!WOS_BUILD_VAR}
+
+if [ ! -f ~/devices/$DEVICE/stock_os/current-stock-os.zip ]; then
+    	echo "Stock OS not found!"
+	echo ""
+	echo "Run \"../stock_os/get-stock-os.sh\" to retreive it."
+else
+	cd ~/devices/$DEVICE/blobs
+
+	# Create the image folder if required.  Stores the final img files to be added to the OTA.
+	if [ ! -d "images" ]; then
+		cd images
+	fi
+
+	# Create the image_raw folder if required.  Stores the raw img files extracted from OOS.
+	if [ ! -d "images_raw" ]; then
+		cd images_raw
+	fi
+
+	# Delete any previous extraction files.
+	rm -rf ~/devices/$DEVICE/blobs/images/*
+
+	# Overall for proprietary blobs we're using TheMuppets, but we still need some of the stock partitions, so get them now.
+	# Make sure we're in the blobs directory to start.
+	cd ~/devices/$DEVICE/blobs/
+
+	# Extract the payload.bin file from stock.
+	unzip -o ~/devices/$DEVICE/stock_os/current-stock-os.zip payload.bin
+
+	# Extract img files so that they can be mounted.
+	python ~/android/lineage-$LOS_BUILD_VERSION/lineage/scripts/update-payload-extractor/extract.py --output_dir ./images_raw payload.bin
+
+	# Change in to the output directory.
+	cd images_raw
+
+	# Get rid of the images we don't need.
+	# 	system/vbmeta/boot/dtbo will be generated during build
+	#	reserve/oem_stanvbk can't be written by lineage recovery
+	rm system.img
+	rm vbmeta.img
+	rm boot.img
+	rm dtbo.img
+	rm reserve.img
+	rm oem_stanvbk.img
+
+	# Change to the images directory.
+	cd ~/devices/$DEVICE/blobs/images
+
+	# Copy over the raw images from OOS.
+	cp ~/devices/$DEVICE/blobs/images_raw/*.img .
+
+	# Remove vendor.img as we'll pull a version with the proper hashtree after the build is run.
+	rm vendor.img
+
+	# Return to the blobs directory.
+	cd ~/devices/$DEVICE/blobs/
+
+	# We don't need payload.bin anymore.
+	rm payload.bin
+fi
---
+++ guacamoleb/build/device-build-functions.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+VENDOR=oneplus
+
+function build_wos {
+	BCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$DEVICE/BoardConfig.mk
+	# For this device we need to add the prebuilt vendor.img and other partitions to the build system, do that now.
+	# First check to see if we've already one it.
+	if ! grep vendor.img $BCFILE > /dev/null; then
+		cat ~/devices/$DEVICE/build/board-config-additions.txt >> $BCFILE
+	fi
+
+	BCCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/sdm845-common/BoardConfigCommon.mk
+	# We need to remove the flag that disables the partition verification during boot if it hasn't been already
+	# in the sdm845 common code.
+	if ! grep "#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS" $BCCFILE > /dev/null; then
+		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2/' $BCCFILE
+	fi
+
+	# Build WOS.
+	common_build_wos
+}
+
+function sign_wos {
+	echo "Start signing process for $DEVICE..."
+
+	# Move in to the build directory
+	cd ~/android/lineage-$LOS_BUILD_VERSION
+
+	# Setup the build environment
+	source build/envsetup.sh
+	croot
+
+	# Use the pre-built version of the vendor img during signing.
+	sign_wos_target_apks_vendor_prebuilt
+
+	# Then generate the OTA as usual.
+	sign_wos_target_files
+
+	# Create the md5 checksum file for the release
+	echo "Create the md5 checksum..."
+	md5sum ~/releases/ota/$PKGNAME.zip > ~/releases/ota/$PKGNAME.zip.md5sum
+
+	# Grab a copy of the build.prop file
+	echo "Store the build.prop file..."
+	cp $OUT/system/build.prop ~/releases/ota/$PKGNAME.zip.prop
+
+	# Cleanup
+	echo "Store signed target files for future incremental updates..."
+	cp signed-target_files.zip ~/releases/signed_files/signed-target_files-$DEVICE-$TODAY.zip
+
+	echo "Signing process complete for $DEVICE!"
+}
+
