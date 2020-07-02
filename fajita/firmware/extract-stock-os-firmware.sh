e367f14858ed6f8bc61b77f15ea39fb04dfd9318
Greg <greg@toolstack.com>
2020-07-02 17:50:50 -0400

    Nullify the firmware extract script...
    as it is no longer needed for newer devices.

--- fajita/firmware/extract-stock-os-firmware.sh
+++ fajita/firmware/extract-stock-os-firmware.sh
@@ -8,20 +8,5 @@ if [ ! -f ../stock_os/current-stock-os.zip ]; then
 	echo ""
         echo "Run \"../stock_os/get-stock-os.sh\" to retreive it."
 else
-	# Cleanup any old updates that exist.
-	rm -rf ~/devices/$DEVICE/firmware/update/*
-
-	# Delete any previous extraction files, needs to be done as root.
-	rm -rf ~/devices/$DEVICE/firmware/output/*
-	rm ~/devices/$DEVICE/firmware/payload.bin
-
-	# Extract the payload.bin file
-	unzip ~/devices/$DEVICE/stock_os/current-stock-os.zip -d ~/devices/$DEVICE/firmware -x payload.bin 
-
-	# Dump the payload.bin in to the output directory
-	python ~/android/lineage/lineage/scripts/update-payload-extractor/extract.py payload.bin --partitions LOGO abl boot dtbo fw_4j1ed fw_fu1ea vbmeta aop bluetooth cmnlib64 cmnlib devcfg dsp hyp keybaster modem qupfw storsec tz xbl_config xbl oem_stanvbk reserve india --output_dir ./
-
-	# Cleanup.
-	rm ~/devices/$DEVICE/firmware/payload.bin
-
+	echo "Firmware is extracted as part of the ../blobs/extract-stock-os-blobs.sh script for $DEVICE."
 fi
