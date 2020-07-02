2bcc5e10b69a4f00df5f21027cbf10826e46c1d7
Greg <greg@toolstack.com>
2020-07-02 17:49:14 -0400

    Housekeeping

--- fajita/stock_os/get-stock-os.sh
+++ fajita/stock_os/get-stock-os.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Don't do anything as OOS 10 has been release and we don't want to upgrade to it yet.
-exit
+# Get the device name from the parent directory of this script's real path.
+DEVICE=$(basename $(dirname $(dirname $(realpath $0))))
 
-cd ~/devices/fajita/stock_os
-curl 'https://www.oneplus.com/xman/send-in-repair/find-phone-systems' -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundaryx6kJsIUK0M2Dzl5d' --data-binary $'------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="storeCode"\r\n\r\nus\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="phoneCode"\r\n\r\nPM1574156215016\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d--\r\n' > oneplus-6t.json
-php ~/devices/dumpling/stock_os/get-oos.php
+# Use curl to download the current info from OnePlus for this device.
+curl 'https://www.oneplus.com/xman/send-in-repair/find-phone-systems' -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundaryx6kJsIUK0M2Dzl5d' --data-binary $'------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="storeCode"\r\n\r\nuk\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="phoneCode"\r\n\r\nPM1574156235282\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d--\r\n' > oneplus.json
+
+# Parse the json file and download/process if neccessary.
+php ~/devices/$DEVICE/stock_os/get-oos.php
