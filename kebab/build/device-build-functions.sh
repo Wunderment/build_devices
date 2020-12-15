8afc0d47cebcf2337685d8ae79b50591bfc15bce
Greg <greg@toolstack.com>
2020-12-15 17:01:50 -0500

    Continue work on kebab support.

--- kebab/build/device-build-functions.sh
+++ kebab/build/device-build-functions.sh
@@ -3,20 +3,6 @@
 VENDOR=oneplus
 
 function build_wos {
-	BCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$DEVICE/BoardConfig.mk
-	# For this device we need to add the prebuilt vendor.img and other partitions to the build system, do that now.
-	# First check to see if we've already one it.
-	if ! grep vendor.img $BCFILE > /dev/null; then
-		cat ~/devices/$DEVICE/build/board-config-additions.txt >> $BCFILE
-	fi
-
-	BCCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/sm8250-common/BoardConfigCommon.mk
-	# We need to remove the flag that disables the partition verification during boot if it hasn't been already
-	# in the sm8250 common code.
-	if ! grep "#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS" $BCCFILE > /dev/null; then
-		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2/' $BCCFILE
-	fi
-
 	# Build WOS.
 	common_build_wos
 }
@@ -32,7 +18,7 @@ function sign_wos {
 	croot
 
 	# Use the pre-built version of the vendor img during signing.
-	sign_wos_target_apks_vendor_prebuilt
+	sign_wos_target_apks
 
 	# Then generate the OTA as usual.
 	sign_wos_target_files
