adb206dae6b0ffa1dba86dae6b9d0dc70c4e6a65
Greg <greg@toolstack.com>
2021-04-12 15:15:53 -0400

    Updates for kebab

--- kebab/build/device-build-functions.sh
+++ kebab/build/device-build-functions.sh
@@ -3,6 +3,22 @@
 VENDOR=oneplus
 
 function build_wos {
+	BCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$DEVICE/BoardConfig.mk
+	# For this device we need to add the prebuilt vendor.img and other partitions to the build system, do that now.
+	# First check to see if we've already one it.
+	if ! grep vendor.img $BCFILE > /dev/null; then
+		cat ~/devices/$DEVICE/build/board-config-additions.txt >> $BCFILE
+	fi
+
+	BCCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/sm8250-common/BoardConfigCommon.mk
+	# We need to remove the flag that disables the partition verification during boot if it hasn't been already
+	# in the sdm845 common code.
+	if ! grep "#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS" $BCCFILE > /dev/null; then
+		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2/' $BCCFILE
+		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag/' $BCCFILE
+		sed -i 's/^BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external\/avb\/test\/data\/testkey_rsa2048.pem/BOARD_AVB_KEY_PATH := \/home\/WundermentOS\/.android-certs\/releasekey.key/' $BCCFILE
+	fi
+
 	DFSFILE=~/android/lineage-$LOS_BUILD_VERSION/kernel/$VENDOR/sm8250/arch/arm64/configs/vendor/kona-perf_defconfig
 	# For this device we need to remove a the debugging file system for our user build.
 	# First check to see if we've already done it.
@@ -24,7 +40,7 @@ function sign_wos {
 	source build/envsetup.sh
 	croot
 
-	# Use the pre-built version of the vendor img during signing.
+	# Use the standard signing script.
 	sign_wos_target_apks
 
 	# Then generate the OTA as usual.
