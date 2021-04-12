f92094930a3e6cd912c0459161e5c933c036f313
Greg <greg@toolstack.com>
2021-04-12 15:15:44 -0400

    Updates for flame

--- flame/build/device-build-functions.sh
+++ flame/build/device-build-functions.sh
@@ -3,17 +3,23 @@
 VENDOR=google
 
 function build_wos {
-	BCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/coral/BoardConfig-lineage.mk
+	BCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/coral/BoardConfigLineage.mk
 	# For this device we need to add the prebuilt vendor.img and other partitions to the build system, do that now.
 	# First check to see if we've already one it.
-	if ! grep vendor.img $BCFILE > /dev/null; then
+	if ! grep WundermentOS $BCFILE > /dev/null; then
 		cat ~/devices/$DEVICE/build/board-config-additions.txt >> $BCFILE
 	fi
 
 	# We need to remove the flag that disables the partition verification during boot.
 	if ! grep "#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS" $BCFILE > /dev/null; then
-		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag/' $BCCFILE
-		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2/' $BCCFILE
+		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag/' $BCFILE
+		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2/' $BCFILE
+	fi
+
+	BCCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/coral/BoardConfig-common.mk
+	# We need to set the correct key for signing the system vbmeta.img.
+	if ! grep "testkey_rsa2048.pem" $BCCFILE > /dev/null; then
+		sed -i 's/^BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external\/avb\/test\/data\/testkey_rsa2048.pem/BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := \/home\/WundermentOS\/.android-certs\/releasekey.key/' $BCCFILE
 	fi
 
 	# Build WOS.
@@ -30,8 +36,8 @@ function sign_wos {
 	source build/envsetup.sh
 	croot
 
-	# Use the pre-built version of the vendor img during signing.
-	sign_wos_target_apks_vendor_prebuilt
+	# Use the standard signing script.
+	sign_wos_target_apks
 
 	# Then generate the OTA as usual.
 	sign_wos_target_files
