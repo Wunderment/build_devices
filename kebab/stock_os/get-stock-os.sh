6c40381a6767239b8580bfd39ed2d0c9d2633e32
Greg <greg@toolstack.com>
2021-04-14 17:46:59 -0400

    Update the script to get the stock os for kebab

--- kebab/stock_os/get-stock-os.sh
+++ kebab/stock_os/get-stock-os.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-# 8T downloads not yet available.
-exit
-
 # Get the device name from the parent directory of this script's real path.
 DEVICE=$(basename $(dirname $(dirname $(realpath $0))))
 
-# Use curl to download the current info from OnePlus for this device.
-curl 'https://www.oneplus.com/xman/send-in-repair/find-phone-systems' -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundaryx6kJsIUK0M2Dzl5d' --data-binary $'------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="storeCode"\r\n\r\nus\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="phoneCode"\r\n\r\nPM1574156235282\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d--\r\n' > oneplus.json
+# A device name may have a special case where we're building multiple versions, like for LOS 16
+# and 17.  In these cases an extra modifier on the device name is added that starts with a '_'
+# so for example dumpling_17 to indicate to build LOS 17 for dumpling.  In these cases we need
+# to leave the modifier on $DEVICE so logs and other commands are executed in the right directory
+# but for the actual LOS build, we need to strip it off.  So do so now.
+LOS_DEVICE=`echo $DEVICE | sed 's/_.*//'`
 
-# Parse the json file and download/process if neccessary.
+# Make sure we're in the stock os directory.
+cd ~/device/$DEVICE/stock_os
+
+# Set the phone code for OnePlus, it is used deep inside the curl call.
+PHONECODE=PM1605596915581
+
+# Remove the old json file before we download the new one.
+rm oneplus.json
+
+# Use curl to download the current info from OnePlus for this device.
+curl 'https://www.oneplus.com/xman/send-in-repair/find-phone-systems' -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundaryx6kJsIUK0M2Dzl5d' --data-binary $'------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="storeCode"\r\n\r\nus\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="phoneCode"\r\n\r\n'"$PHONECODE"$'\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d--\r\n' -o oneplus.json
+
+# Parse the json file and download/process if necessary.
 php ~/devices/$DEVICE/stock_os/get-oos.php
