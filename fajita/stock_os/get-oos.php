4f71a0d1e422830e522b841c46d4916e77dbc8c0
Greg <greg@toolstack.com>
2021-04-14 22:28:27 -0400

    Updated OnePlus stock os scripts

--- fajita/stock_os/get-oos.php
+++ fajita/stock_os/get-oos.php
@@ -1,4 +1,16 @@
 <?php
+// Set what device we're downloading for.
+$device = basename( dirname( realpath( getcwd() ) ) );;
+
+/*
+ * A device name may have a special case where we're building multiple versions, like for LOS 16
+ * and 17.  In these cases an extra modifier on the device name is added that starts with a '_'
+ * so for example dumpling_17 to indicate to build LOS 17 for dumpling.  In these cases we need
+ * to leave the modifier on $DEVICE so logs and other commands are executed in the right directory
+ * but for the actual LOS build, we need to strip it off.  So do so now.
+ */
+$los_device=preg_replace( '/_.*/', '', $device );
+
 // Pull the current stats from OnePlus.com and decode the json response.
 $string = file_get_contents( 'oneplus.json' );
 $json = json_decode( $string, true );
@@ -12,15 +24,15 @@ foreach( $json['data'] as $field ) {
 }
 
 // Pull in the date value from the last time we checked.
-$last_filename = '/home/WundermentOS/devices/fajita/stock_os/last.stock.os.release.txt';
+$last_filename = '/home/WundermentOS/devices/' . $device . '/stock_os/last.stock.os.release.txt';
 $last_release = intval( file_get_contents( $last_filename ) );
 
 // Check if it's different from the value that OnePlus.com just returned to us.
 if( $current_release[ 'versionReleaseTime' ] !== $last_release ) {
 	// If so, download the new release.
-	echo 'New release found: ' . $current_release[ 'versionNo' ] . PHP_EOL;
+	echo "New release found for $los_device:" . PHP_EOL;
 
-	$cmd = 'wget -O ~/devices/fajita/stock_os/current-stock-os.zip ' . escapeshellarg( $current_release[ 'versionLink' ] );
+	$cmd = 'wget -O ~/devices/' . $device . '/stock_os/current-stock-os.zip ' . escapeshellarg( $current_release[ 'versionLink' ] );
 
 	exec( $cmd );
 
@@ -33,7 +45,5 @@ if( $current_release[ 'versionReleaseTime' ] !== $last_release ) {
 	// Extract the firmware.
 	exec( '../firmware/extract-stock-os-firmware.sh' );
 } else {
-	echo 'No new release found.' . PHP_EOL;
+	echo "No new release found for $los_device." . PHP_EOL;
 }
-
-
--- kebab/stock_os/get-stock-os.sh
+++ kebab/stock_os/get-stock-os.sh
@@ -15,12 +15,13 @@ cd ~/device/$DEVICE/stock_os
 
 # Set the phone code for OnePlus, it is used deep inside the curl call.
 PHONECODE=PM1605596915581
+PHONELOC=us
 
 # Remove the old json file before we download the new one.
 rm oneplus.json
 
 # Use curl to download the current info from OnePlus for this device.
-curl 'https://www.oneplus.com/xman/send-in-repair/find-phone-systems' -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundaryx6kJsIUK0M2Dzl5d' --data-binary $'------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="storeCode"\r\n\r\nus\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="phoneCode"\r\n\r\n'"$PHONECODE"$'\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d--\r\n' -o oneplus.json
+curl 'https://www.oneplus.com/xman/send-in-repair/find-phone-systems' -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundaryx6kJsIUK0M2Dzl5d' --data-binary $'------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="storeCode"\r\n\r\n'"$PHONELOC"$'\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d\r\nContent-Disposition: form-data; name="phoneCode"\r\n\r\n'"$PHONECODE"$'\r\n------WebKitFormBoundaryx6kJsIUK0M2Dzl5d--\r\n' -o oneplus.json
 
 # Parse the json file and download/process if necessary.
 php ~/devices/$DEVICE/stock_os/get-oos.php
