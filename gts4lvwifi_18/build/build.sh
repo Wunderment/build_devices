2a92f2ddd6ced77705798141b99d0bbdb1c8cbd0
Greg <greg@toolstack.com>
2021-04-23 15:36:44 -0400

    Update device build script for new global build script options

--- gts4lvwifi_18/build/build.sh
+++ gts4lvwifi_18/build/build.sh
@@ -5,11 +5,8 @@
 # Get the device name from the parent directory of this script's real path.
 DEVICE=$(basename $(dirname $(dirname $(realpath $0))))
 
-# Get the build action from the first command line parameter.
-WOS_BUILD_TYPE=$1
-
 # Export a proper script title so when we call the real build script, if there is an error, it will give the correct information.
-export SCRIPT_TITLE="Usage: ./build.sh <action>"
+export SCRIPT_TITLE="Usage: ./build.sh <action> ... <action>"
 
-~/tasks/build/build.sh $WOS_BUILD_TYPE $DEVICE
+~/tasks/build/build.sh $@ $DEVICE
 
