c43f187db5ca8bb5ed1bb494985eaec5ad384822
Greg <greg@toolstack.com>
2021-04-28 20:59:47 -0400

    Add LineageOS 18 device for enchilada

---
+++ enchilada_18/firmware/extract-stock-os-firmware.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Configuration variables for this device.
+VENDOR=oneplus
+DELETE_IMAGES="system.img vbmeta.img boot.img dtbo.img india.img reserve.img"
+PREBUILT_VENDOR=true
+
+source ~/tasks/firmware/extract-stock-os-firmware-common.sh
