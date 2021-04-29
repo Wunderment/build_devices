9a637ea0499f7b716923a2cce78f8b8271b0370f
Greg <greg@toolstack.com>
2021-04-28 22:35:50 -0400

    Updated kebab delete images list

--- kebab/firmware/extract-stock-os-firmware.sh
+++ kebab/firmware/extract-stock-os-firmware.sh
@@ -2,7 +2,7 @@
 
 # Configuration variables for this device.
 VENDOR=oneplus
-DELETE_IMAGES="system.img system_ext.img vbmeta.img vbmeta_system.img boot.img dtbo.img india.img reserve.img oem_stanvbk.img product.img recovery.img vendor.img"
+DELETE_IMAGES="system.img system_ext.img vbmeta.img vbmeta_system.img boot.img dtbo.img product.img recovery.img vendor.img"
 PREBUILT_VENDOR=false
 
 source ~/tasks/firmware/extract-stock-os-firmware-common.sh
