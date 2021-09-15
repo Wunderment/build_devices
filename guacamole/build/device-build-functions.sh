9b64e861c13c7ddd1aae6ee5977122b3e8d716c2
Greg <greg@toolstack.com>
2021-09-14 22:44:12 -0400

    Update grep search pattern.

--- guacamole/build/device-build-functions.sh
+++ guacamole/build/device-build-functions.sh
@@ -6,7 +6,7 @@ function build_wos {
 	BCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$LOS_DEVICE/BoardConfig.mk
 	# For this device we need to add the prebuilt vendor.img and other partitions to the build system, do that now.
 	# First check to see if we've already one it.
-	if ! grep vendor.img $BCFILE > /dev/null; then
+	if ! grep ADD_RADIO_FILES $BCFILE > /dev/null; then
 		cat ~/devices/$DEVICE/build/board-config-additions.txt >> $BCFILE
 	fi
 
