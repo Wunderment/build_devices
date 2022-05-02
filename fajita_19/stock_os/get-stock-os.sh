44f9c8484a4a05a98183fd7ada9d631d84348db0
Greg <greg@toolstack.com>
2022-05-02 13:42:45 -0400

    Add fajita (Oneplus 6T) LineageOS 12 build device

---
+++ fajita_19/stock_os/get-stock-os.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Don't bother checking for new releases as the 6T is out of support now.
+exit
+
+# Get the device name from the parent directory of this script's real path.
+DEVICE=$(basename $(dirname $(dirname $(realpath $0))))
+
+# A device name may have a special case where we're building multiple versions, like for LOS 16
+# and 17.  In these cases an extra modifier on the device name is added that starts with a '_'
+# so for example dumpling_17 to indicate to build LOS 17 for dumpling.  In these cases we need
+# to leave the modifier on $DEVICE so logs and other commands are executed in the right directory
+# but for the actual LOS build, we need to strip it off.  So do so now.
+LOS_DEVICE=`echo $DEVICE | sed 's/_.*//'`
+
+# Make sure we're in the stock os directory.
+cd ~/devices/$DEVICE/stock_os
+
+# Set the phone code and other settings to use in get-oneplus.sh.
+PHONECODE=PM1574156215016
+PHONELOC=ca
+COOKIES=refererstore=ca_en; redirectedCountry=ca_en;
+
+# Call the common script to download the stock OS from OnePlus.
+source /home/WundermentOS/tasks/stock_os/get-oneplus.sh
