0e2d57471b8c5a1b32e8133a11305388457b22ba
Greg <greg@toolstack.com>
2022-06-18 23:10:31 -0400

    Fix OTA updater permissions

--- lemonade_18/build/device-build-functions.sh
+++ lemonade_18/build/device-build-functions.sh
@@ -51,6 +51,12 @@ function build_wos {
 		patch $IQFILE ~/devices/$DEVICE/build/init.qcom.rc.patch
 	fi
 
+	TEFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/sm8350-common/sepolicy/vendor/update_engine.te
+	# Add in the extra permissions for the update engine to access the extra oem partitions for OTAs.
+	if [ ! -f $TEFILE ]; then
+		cp ~/devices/$DEVICE/build/update_engine.te $TEFILE
+	fi
+
 	# Build WOS.
 	common_build_wos
 }
