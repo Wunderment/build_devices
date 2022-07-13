d5b8a6a4fcd02028231666b03065c9b4b0c21475
Greg <greg@toolstack.com>
2022-07-13 15:05:56 -0400

    Move to newer cleanup function

--- gts4lvwifi_19/build/device-build-functions.sh
+++ gts4lvwifi_19/build/device-build-functions.sh
@@ -17,17 +17,8 @@ function sign_wos {
 	# Call the common tasks of creating the target files package from the global build functions.
 	sign_wos_target_package
 
-	# Create the md5 checksum file for the release
-	echo "Create the md5 checksum..."
-	md5sum ~/releases/ota/$LOS_DEVICE/$PKGNAME.zip > ~/releases/ota/$LOS_DEVICE/$PKGNAME.zip.md5sum
-
-	# Grab a copy of the build.prop file
-	echo "Store the build.prop file..."
-	cp $OUT/system/build.prop ~/releases/ota/$LOS_DEVICE/$PKGNAME.zip.prop
-
-	# Cleanup
-	echo "Store signed target files for future incremental updates..."
-	cp signed-target_files.zip ~/releases/signed_files/$LOS_DEVICE/signed-target_files-$LOS_DEVICE-$TODAY.zip
+	# Create the MD5 checksum file, copy the build prop file and cleanup the target_files zip.
+	checksum_buildprop_cleanup
 
 	echo "Signing process complete for $DEVICE!"
 }
