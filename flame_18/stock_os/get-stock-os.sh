3d8a964ae12534ce5d731ff36f5fedd92ea9fb5f
Greg <greg@toolstack.com>
2022-10-21 21:02:38 -0400

    Update the stock os scripts for google devices for new OTA downloader

--- flame_18/stock_os/get-stock-os.sh
+++ flame_18/stock_os/get-stock-os.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-# Get the device name from the parent directory of this script's real path.
-DEVICE=$(basename $(dirname $(dirname $(realpath $0))))
-
-# A device name may have a special case where we're building multiple versions, like for LOS 16
-# and 17.  In these cases an extra modifier on the device name is added that starts with a '_'
-# so for example dumpling_17 to indicate to build LOS 17 for dumpling.  In these cases we need
-# to leave the modifier on $DEVICE so logs and other commands are executed in the right directory
-# but for the actual LOS build, we need to strip it off.  So do so now.
-LOS_DEVICE=`echo $DEVICE | sed 's/_.*//'`
-
-# Make sure we're in the stock os directory.
-cd ~/devices/$DEVICE/stock_os
+# Define where the security patch is stored for this device, rooted at "lineageos/device/google".
+SECURITY_PATCH_FILE=coral/device-common.mk
 
 source ~/tasks/stock_os/get-pixel.sh
