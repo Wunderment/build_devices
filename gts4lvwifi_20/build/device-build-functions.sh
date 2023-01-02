d176bcbcef8e6058ba2403c32d1db5da09c357f7
Greg <greg@toolstack.com>
2023-01-02 14:42:38 -0500

    Add recovery to the images flashed during updates.

--- gts4lvwifi_20/build/device-build-functions.sh
+++ gts4lvwifi_20/build/device-build-functions.sh
@@ -5,7 +5,22 @@
 # Tell the build system which img file to use as the recovery img.
 export LOS_RECOVERY_IMG=recovery
 
+### Local variables ###
+
+# Set the vendor for this device.
+VENDOR=samsung
+
+# This device use a common/shared device tree.
+COMMONDEVICE=gts4lv-common
+
 function build_wos {
+	RTFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$COMMONDEVICE/releasetools.py
+	# For this device we need to add the recovery.img to the flashing script.
+	# First check to see if we've already one it.
+	if ! grep recovery.img $RTFILE > /dev/null; then
+		sed -i 's/^def OTA_InstallEnd(info):/def OTA_InstallEnd(info):\n  AddImage(info, "recovery.img", "\/dev\/block\/bootdevice\/by-name\/recovery")/' $RTFILE
+	fi
+
 	common_build_wos
 }
 
