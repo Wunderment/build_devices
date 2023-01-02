9b96c8cc3cfeb37810e1113918a0e51270ca5dc0
Greg <greg@toolstack.com>
2023-01-02 14:00:57 -0500

    Add LineageOS 20 build target for flame.

---
+++ flame_20/stock_os/get-stock-os.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Define where the security patch is stored for this device, rooted at "lineageos/device/google".
+SECURITY_PATCH_FILE=coral/device-common.mk
+
+source ~/tasks/stock_os/get-pixel.sh
