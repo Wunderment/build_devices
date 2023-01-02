1e845df67da642ebb5c8d8448c2eee4f283c6162
Greg <greg@toolstack.com>
2023-01-01 23:53:15 -0500

    Add LineageOS 20 build target for fajita.

---
+++ fajita_20/build/device-build-functions.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+### Global variables ###
+
+# Tell the build system which img file to use as the recovery img.
+export LOS_RECOVERY_IMG=boot
+
+### Local variables ###
+
+# Set the vendor for this device.
+VENDOR=oneplus
+
+# This device use a common/shared device tree.
+COMMONDEVICE=sdm845-common
+
+function build_wos {
+	BCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$LOS_DEVICE/BoardConfig.mk
+	# For this device we need to add the prebuilt vendor.img and other partitions to the build system, do that now.
+	# First check to see if we've already one it.
+	if ! grep vendor.img $BCFILE > /dev/null; then
+		cat ~/devices/$DEVICE/build/board-config-additions.txt >> $BCFILE
+	fi
+
+	BCCFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$COMMONDEVICE/BoardConfigCommon.mk
+	# We need to remove the flag that disables the partition verification during boot if it hasn't been already
+	# in the sdm845 common code.
+	if ! grep "#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS" $BCCFILE > /dev/null; then
+		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2/' $BCCFILE
+		sed -i 's/^BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag/#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag/' $BCCFILE
+	fi
+
+	CFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$COMMONDEVICE/common.mk
+	# We need to add the OEM lock/unlock feature to developers options if it's not there already.
+	if ! grep "ro.oem_unlock_supported=1" $CFILE > /dev/null; then
+		sed -i 's/^# OnePlus/# OEM Unlock reporting\nPRODUCT_DEFAULT_PROPERTY_OVERRIDES += \\\n    ro.oem_unlock_supported=1\n\n# OnePlus/' $CFILE
+	fi
+
+	ABFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$LOS_DEVICE/AndroidBoard.mk
+	# Add the RADIO files to the build system.
+	if [ ! -f $ABFILE ]; then
+		cp  ~/devices/$DEVICE/build/AndroidBoard.mk $ABFILE
+	fi
+
+	IRQFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$COMMONDEVICE/rootdir/etc/init.recovery.qcom.rc
+	# We need to add a couple of symlinks to the recovery init script so we can flash partitions.
+	if ! grep "oem_stanvbk_a" $IRQFILE > /dev/null; then
+		patch $IRQFILE ~/devices/$DEVICE/build/init.recovery.qcom.rc.patch
+	fi
+
+	IQFILE=~/android/lineage-$LOS_BUILD_VERSION/device/$VENDOR/$COMMONDEVICE/rootdir/etc/init.qcom.rc
+	# We need to add a couple of symlinks to the init script so we can flash partitions.
+	if ! grep "oem_stanvbk_a" $IQFILE > /dev/null; then
+		patch $IQFILE ~/devices/$DEVICE/build/init.qcom.rc.patch
+	fi
+
+	# Build WOS.
+	common_build_wos
+}
+
+function sign_wos {
+	echo "Start signing process for $DEVICE..."
+
+	# Move in to the build directory
+	cd ~/android/lineage-$LOS_BUILD_VERSION
+
+	# Setup the build environment
+	source build/envsetup.sh
+	croot
+
+	# Use the pre-built version of the vendor IMG during signing.
+	sign_wos_target_apks_vendor_prebuilt
+
+	# Then generate the OTA as usual.
+	sign_wos_target_files
+
+	# Create the MD5 checksum file, copy the build prop file and cleanup the target_files zip.
+	checksum_buildprop_cleanup
+
+	echo "Signing process complete for $DEVICE!"
+}
+
