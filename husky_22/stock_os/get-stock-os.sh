6c23c62cf3b52d6f73384c4da750004ad88c8017
Greg Ross <greg@toolstack.com>
2025-02-18 18:35:35 -0500

    Add husky build for Lineage 22

---
+++ husky_22/stock_os/get-stock-os.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Exit as we're using the muppets.
+exit
+
+# Define where the security patch is stored for this device, rooted at "lineageos/device/google".
+SECURITY_PATCH_FILE=gs201/device.mk
+
+source ~/tasks/stock_os/get-pixel.sh
